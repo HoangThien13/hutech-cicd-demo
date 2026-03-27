ba9123762fd4bc615f5d45be24871a2a4cf36fbd
HoangThien13 <th1352004@gmail.com>
2026-03-27 10:34:55 +0700

    Update HelloController.java

--- src/main/java/com/example/NguyenHoangThien/HelloController.java
+++ src/main/java/com/example/NguyenHoangThien/HelloController.java
@@ -1,12 +1,12 @@
 package com.example.NguyenHoangThien;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Chào Thiện! App Spring Boot đã chạy thành công trong Docker rồi nhé!";
+        return "index"; // Trả về tên file index.html trong thư mục templates
     }
 }
